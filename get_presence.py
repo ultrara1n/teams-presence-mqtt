5fcf9e8afa3ed58399a6800ae90af478aafe4ab5
Philipp Wensauer <mail@philippwensauer.com>
2020-12-23 09:25:33 +0100

    loop

--- get_presence.py
+++ get_presence.py
@@ -4,13 +4,16 @@ import time
 import os
 import paho.mqtt.client as mqtt
 
+
 def getAccessToken(clientId, refreshToken, redirectUri):
     grant_type = 'refresh_token'
     scope = 'offline_access presence.read'
 
-    payload = {'client_id' : clientId, 'refresh_token' : refreshToken, 'grant_type' : grant_type, 'scope' : scope, 'redirect_uri' : redirectUri}
+    payload = {'client_id': clientId, 'refresh_token': refreshToken,
+               'grant_type': grant_type, 'scope': scope, 'redirect_uri': redirectUri}
 
-    getAccessToken = requests.post('https://login.microsoftonline.com/organizations/oauth2/v2.0/token', data=payload)
+    getAccessToken = requests.post(
+        'https://login.microsoftonline.com/organizations/oauth2/v2.0/token', data=payload)
 
     response = getAccessToken.json()
 
@@ -20,7 +23,8 @@ def getAccessToken(clientId, refreshToken, redirectUri):
 
     saveToken(accessTokenExpireDate, response['access_token'])
 
-    return response['access_token'] 
+    return response['access_token']
+
 
 def saveToken(expireTimestamp, accessToken):
     tokenData = {}
@@ -31,10 +35,12 @@ def saveToken(expireTimestamp, accessToken):
     with open('token.json', 'w') as outfile:
         json.dump(tokenData, outfile)
 
-def getPresence(accessToken):
-    presenceRequestHeaders = {'Authorization' : 'Bearer ' + accessToken}
 
-    getPresence = requests.get('https://graph.microsoft.com/v1.0/me/presence', headers=presenceRequestHeaders)
+def getPresence(accessToken):
+    presenceRequestHeaders = {'Authorization': 'Bearer ' + accessToken}
+
+    getPresence = requests.get(
+        'https://graph.microsoft.com/v1.0/me/presence', headers=presenceRequestHeaders)
 
     presenceResponseJson = getPresence.json()
 
@@ -44,30 +50,38 @@ def getPresence(accessToken):
 
     return presenceData
 
-#load settings
+
+# load settings
 settingsFile = open('settings.json', 'r')
 settingsJson = json.loads(settingsFile.read())
 
 mqttClient = mqtt.Client()
-mqttClient.username_pw_set(settingsJson['mqtt_username'], settingsJson['mqtt_password'])
+mqttClient.username_pw_set(
+    settingsJson['mqtt_username'], settingsJson['mqtt_password'])
 mqttClient.connect(settingsJson['mqtt_host'], settingsJson['mqtt_port'], 60)
 mqttClient.loop_start()
 
-if os.path.isfile('token.json'):
-    tokenFile = open('token.json', 'r')
-    try:
-        tokenJson = json.loads(tokenFile.read())
+while True:
+    if os.path.isfile('token.json'):
+        tokenFile = open('token.json', 'r')
+        try:
+            tokenJson = json.loads(tokenFile.read())
 
-        if tokenJson['expire_timestamp'] <= time.time():
-            accessToken = getAccessToken(settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
-        else:
-            accessToken = tokenJson['access_token']
+            if tokenJson['expire_timestamp'] <= time.time():
+                accessToken = getAccessToken(
+                    settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
+            else:
+                accessToken = tokenJson['access_token']
 
-    except ValueError:
-        accessToken = getAccessToken(settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
-else:
-    accessToken = getAccessToken(settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
+        except ValueError:
+            accessToken = getAccessToken(
+                settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
+    else:
+        accessToken = getAccessToken(
+            settingsJson['client_id'], settingsJson['refresh_token'], settingsJson['redirect_uri'])
 
-presenceData = getPresence(accessToken)
+    presenceData = getPresence(accessToken)
 
-mqttClient.publish('home/buero/teams', json.dumps(presenceData))
+    mqttClient.publish('home/buero/teams', json.dumps(presenceData))
+
+    time.sleep(settingsJson['check_every'])
